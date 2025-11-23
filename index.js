8869ed4bee3cf525facbce5f51fb3c0c3b659d02
Sohanur Rahman <sohanurrahman3092@gmail.com>
2025-11-23 22:01:07 +0600

    upto add product and myImport done

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const client = new MongoClient(uri, {
 
 const verifyTokenMiddleware = async (req, res, next) => {
   const authorization = req.headers.authorization;
-  
+
   if (!authorization) {
     return res.status(401).send({
       message: "Unauthorized Access, Token not Found."
@@ -45,12 +45,12 @@ const verifyTokenMiddleware = async (req, res, next) => {
   const token = authorization.split(' ')[1]
   // console.log(token);
 
-  try{
+  try {
     const decode = await admin.auth().verifyIdToken(token);
     console.log(decode);
     next();
   }
-  catch (error){
+  catch (error) {
     res.status(401).send({
       message: "Unauthorized Access."
     })
@@ -66,39 +66,39 @@ async function run() {
     const productsCollection = db.collection('products')
     const importsCollection = db.collection('imports')
 
-    app.get('/products', async (req,res) => {
+    app.get('/products', async (req, res) => {
       const result = await productsCollection.find().toArray();
       res.send(result);
     })
 
     app.get('/latest-products', async (req, res) => {
-      const cursor = productsCollection.find().sort({created_at: -1}).limit(6);
+      const cursor = productsCollection.find().sort({ created_at: -1 }).limit(6);
       const result = await cursor.toArray();
       res.send(result);
     })
 
     app.get(`/productDetails/:id`, async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await productsCollection.findOne(query);
       res.send(result);
-    }) 
+    })
 
     app.get('/search', async (req, res) => {
       const searchedText = req.query.search;
-      const result = await productsCollection.find({productName: {$regex: searchedText, $options: "i"}}).toArray();
+      const result = await productsCollection.find({ productName: { $regex: searchedText, $options: "i" } }).toArray();
       res.send(result);
     })
 
     app.get(`/product/:id`, async (req, res) => {
-      const {id} = req.params;
-      const result = await productsCollection.findOne({_id: new ObjectId(id)});
+      const { id } = req.params;
+      const result = await productsCollection.findOne({ _id: new ObjectId(id) });
       res.send(result);
     })
 
     app.get('/myImports', verifyTokenMiddleware, async (req, res) => {
       const email = req.query.email;
-      const result = await importsCollection.find({imported_by: email}).toArray();
+      const result = await importsCollection.find({ imported_by: email }).toArray();
       res.send(result);
     })
 
@@ -108,23 +108,48 @@ async function run() {
       const quantity = Number(importData.quantity);
       const result = await importsCollection.insertOne(importData);
 
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const update = {
         $inc: {
           availableQuantity: - quantity
         }
       }
       const newImpQuantity = await productsCollection.updateOne(filter, update);
-      res.send({result, newImpQuantity});
+      res.send({ result, newImpQuantity });
     })
 
     app.post('/products', verifyTokenMiddleware, async (req, res) => {
-      const query = req.body;
-      const result = await productsCollection.insertOne(query);
-      res.send({
-        success: true,
-        result,
-      })
+      const body = req.body;
+      if (
+        isNaN(body.availableQuantity) ||
+        isNaN(body.price) ||
+        isNaN(body.rating)
+      ) {
+        return res.status(400).send({
+          message: "Quantity, price, and rating must be numbers"
+        });
+      }
+
+      try {
+        const result = await productsCollection.insertOne(body);
+        res.send({
+          success: true,
+          message: "Product added successfully",
+          insertedId: result.insertedId,
+        });
+      } catch (error) {
+        console.error("Database insertion failed:", error);
+        res.status(500).send({
+          success: false,
+          message: "Failed to insert product due to a database error."
+        });
+      }
+    })
+
+    app.delete('/product/:id', verifyTokenMiddleware, async (req, res) => {
+      const id = req.params.id;
+      const result = await importsCollection.deleteOne({ _id: new ObjectId(id) })
+      res.send(result)
     })
 
 
