5e4e4f2be039b52488ed5d7154a6c895bbe282ed
Sohanur Rahman <sohanurrahman3092@gmail.com>
2025-11-25 17:38:17 +0600

    Complete

--- index.js
+++ index.js
@@ -199,7 +199,6 @@ async function run() {
       })
     })
 
-
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
